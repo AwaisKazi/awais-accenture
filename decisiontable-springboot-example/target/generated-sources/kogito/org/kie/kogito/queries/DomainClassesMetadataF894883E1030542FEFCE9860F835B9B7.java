36433bbcd769aab18160819071eb566661c47efc
Jimmy Ellis <jimmy.ellisx@outlook.com>
2021-10-26 17:21:02 -0500

    all

--- decisiontable-springboot-example/target/generated-sources/kogito/org/kie/kogito/queries/DomainClassesMetadataF894883E1030542FEFCE9860F835B9B7.java
+++ decisiontable-springboot-example/target/generated-sources/kogito/org/kie/kogito/queries/DomainClassesMetadataF894883E1030542FEFCE9860F835B9B7.java
@@ -1,33 +1,6 @@
 package org.kie.kogito.queries;
 public class DomainClassesMetadataF894883E1030542FEFCE9860F835B9B7 {
 
-    public static final org.drools.model.DomainClassMetadata org_kie_kogito_queries_SchoolGrades_Metadata_INSTANCE = new org_kie_kogito_queries_SchoolGrades_Metadata();
-    private static class org_kie_kogito_queries_SchoolGrades_Metadata implements org.drools.model.DomainClassMetadata {
-
-        @Override
-        public Class<?> getDomainClass() {
-            return org.kie.kogito.queries.SchoolGrades.class;
-        }
-
-        @Override
-        public int getPropertiesSize() {
-            return 6;
-        }
-
-        @Override
-        public int getPropertyIndex( String name ) {
-            switch(name) {
-                case "applicant": return 0;
-                case "average": return 1;
-                case "biologyGrade": return 2;
-                case "chemistryGrade": return 3;
-                case "mathGrade": return 4;
-                case "passed": return 5;
-             }
-             throw new RuntimeException("Unknown property '" + name + "' for class class class org.kie.kogito.queries.SchoolGrades");
-        }
-    }
-
     public static final org.drools.model.DomainClassMetadata java_lang_String_Metadata_INSTANCE = new java_lang_String_Metadata();
     private static class java_lang_String_Metadata implements org.drools.model.DomainClassMetadata {
 
@@ -74,6 +47,33 @@ public class DomainClassesMetadataF894883E1030542FEFCE9860F835B9B7 {
                 case "id": return 3;
              }
              throw new RuntimeException("Unknown property '" + name + "' for class class class org.kie.kogito.queries.CreditCardApplication");
+        }
+    }
+
+    public static final org.drools.model.DomainClassMetadata org_kie_kogito_queries_SchoolGrades_Metadata_INSTANCE = new org_kie_kogito_queries_SchoolGrades_Metadata();
+    private static class org_kie_kogito_queries_SchoolGrades_Metadata implements org.drools.model.DomainClassMetadata {
+
+        @Override
+        public Class<?> getDomainClass() {
+            return org.kie.kogito.queries.SchoolGrades.class;
+        }
+
+        @Override
+        public int getPropertiesSize() {
+            return 6;
+        }
+
+        @Override
+        public int getPropertyIndex( String name ) {
+            switch(name) {
+                case "applicant": return 0;
+                case "average": return 1;
+                case "biologyGrade": return 2;
+                case "chemistryGrade": return 3;
+                case "mathGrade": return 4;
+                case "passed": return 5;
+             }
+             throw new RuntimeException("Unknown property '" + name + "' for class class class org.kie.kogito.queries.SchoolGrades");
         }
     }
 
--- decisiontable-springboot-example/target/generated-sources/kogito/org/kie/kogito/queries/RulesF894883E1030542FEFCE9860F835B9B7_GradeUnit.java
+++ decisiontable-springboot-example/target/generated-sources/kogito/org/kie/kogito/queries/RulesF894883E1030542FEFCE9860F835B9B7_GradeUnit.java
@@ -22,7 +22,7 @@ public class RulesF894883E1030542FEFCE9860F835B9B7_GradeUnit extends RulesF89488
 
     /**
      * With the following expression ID:
-     * org.drools.modelcompiler.builder.generator.DRLIdGenerator@6a118ec6
+     * org.drools.modelcompiler.builder.generator.DRLIdGenerator@119bf41f
      */
     @Override
     public java.util.List<org.drools.model.Rule> getRules() {
